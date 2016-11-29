fb9b21d408047470f9ae925618646e95a54b9528
miewsukanya <miewsukanya.24@gmail.com>
2016-11-29 21:42:12 +0700

    Create Main Login FindSignAdmin Success

---
+++ app/src/main/java/miewsukanya/com/findsignadmin/MyAlert.java
@@ -0,0 +1,43 @@
+package miewsukanya.com.findsignadmin;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by Namsom on 26/11/2559.
+ */
+
+public class MyAlert {
+
+    //Explicit
+    private Context context;
+    private int anInt;
+    private String titleString, messageString;
+
+    public MyAlert(Context context, int anInt, String titleString, String messageString) {
+        this.context = context;
+        this.anInt = anInt;
+        this.titleString = titleString;
+        this.messageString = messageString;
+    }
+
+    public void myDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(anInt);
+        builder.setTitle(titleString);
+        builder.setMessage(messageString);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }   // myDialog
+
+
+}
---
+++ app/src/main/java/miewsukanya/com/findsignadmin/MyConstant.java
@@ -0,0 +1,17 @@
+package miewsukanya.com.findsignadmin;
+
+/**
+ * Created by Namsom on 26/11/2559.
+ */
+
+public class MyConstant {
+
+    //Expcilit
+    private String urlGetJSON = "http://202.28.94.32/2559/563020232-9/get_user.php";
+
+    public String getUrlGetJSON() {
+        return urlGetJSON;
+    }
+
+
+}
