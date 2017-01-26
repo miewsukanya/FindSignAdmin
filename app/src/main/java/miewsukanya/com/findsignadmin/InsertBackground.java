38d5d55a73929cf8baefa0b9ec591841b6cc4873
miewsukanya <miewsukanya.24@gmail.com>
2017-01-26 22:11:36 +0700

    insert background create success

---
+++ app/src/main/java/miewsukanya/com/findsignadmin/InsertBackground.java
@@ -0,0 +1,96 @@
+package miewsukanya.com.findsignadmin;
+
+/**
+ * Created by Sukanya Boonpun on 26/01/2560.
+ */
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by Sukanya Boonpun on 21/09/2559.
+ */
+public class InsertBackground extends AsyncTask<String, Void, String> {
+    Context context;
+    AlertDialog alertDialog;
+    InsertBackground (Context ctx){
+        context = ctx;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String type = params[0];
+        //url จากของ server
+        String insert_url = "http://202.28.94.32/2559/563020232-9/add2.php";
+        if(type.equals("insert")){
+            try {
+                String str_signname = params[1];
+                String str_latitude = params[2];
+                String str_longitude = params[3];
+                //String str_adid = params[4];
+                URL url= new URL(insert_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                String post_data = URLEncoder.encode("SignName","UTF-8")+"="+URLEncoder.encode(str_signname,"UTF-8")+"&"
+                        +URLEncoder.encode("Latitude","UTF-8")+"="+URLEncoder.encode(str_latitude,"UTF-8")+"&"
+                        +URLEncoder.encode("Longitude","UTF-8")+"="+URLEncoder.encode(str_longitude,"UTF-8");
+                //SignName,Latitude,Longitude,AdID ชื่อคอลัมในฐานข้อมูล
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                String result="";
+                String line="";
+                while ((line = bufferedReader.readLine())!=null){
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return  result;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Insert Status");
+
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
+
+    }
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}//main class
